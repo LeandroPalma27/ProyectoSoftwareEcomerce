6a30316b6b65c74ed65ae8b9b28d2bc5f88deb82
Leandro <psychomusicbox@gmail.com>
2021-10-26 17:18:32 -0500

    Cantidad en vista index Fetch

--- Evaluacion4/wwwroot/js/addAndRemoveitems.js
+++ Evaluacion4/wwwroot/js/addAndRemoveitems.js
@@ -1,3 +1,33 @@
+// var btnRemove, btnAdd, inputCantidad, inputCantidadNumber;
+// inputCantidadNumber = 0;
+
+// btnRemove = document.getElementsByClassName('cantidad_btn-remove');
+// btnAdd = document.getElementsByClassName('cantidad_btn-add');
+// inputCantidad = document.getElementsByClassName('input-cantidad');
+
+// [].forEach.call(btnRemove, function (e) {
+//     e.onclick = function () {
+//         let tamañoArrayInputsCantidad = inputCantidad.length;
+//         let contador = 0;
+//         while (contador <= (tamañoArrayInputsCantidad)) {
+//             contador++;
+//             if (inputCantidad[contador].value < 100) {
+//                 inputCantidadNumber++;
+//                 inputCantidad[contador].innerHTML = inputCantidadNumber;
+//                 inputCantidad[contador].value = inputCantidadNumber;
+//             }
+//             break;
+//         }
+
+//         for (var i = 0; i < inputCantidad.length; i++) {
+
+//         }
+//         [].forEach.call(inputCantidad, function (e) {
+
+//         });
+//     }
+// });
+
 var btnRemove, btnAdd, inputCantidad;
 
 btnRemove = document.getElementById('btnRemove');
@@ -21,7 +51,7 @@ btnRemove.addEventListener('click', function (e) {
 });
 
 btnAdd.addEventListener('click', function (e) {
-    if (inputCantidadNumber == 99 || inputCantidadNumber > 100) {
+    if (inputCantidadNumber > 99) {
         alert('Cantidad excedida (solo 99 productos por compra)');
     } else {
         inputCantidadNumber++;
@@ -29,3 +59,14 @@ btnAdd.addEventListener('click', function (e) {
         inputCantidad.value = inputCantidadNumber;
     }
 });
+
+
+// btnAdd.addEventListener('click', function (e) {
+//     if (inputCantidadNumber == 99 || inputCantidadNumber > 100) {
+//         alert('Cantidad excedida (solo 99 productos por compra)');
+//     } else {
+//         inputCantidadNumber++;
+//         inputCantidad.innerHTML = inputCantidadNumber;
+//         inputCantidad.value = inputCantidadNumber;
+//     }
+// });
