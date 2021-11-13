0702ee0343e2184e5296de1e9283e73ec6c30225
Leandro <psychomusicbox@gmail.com>
2021-11-13 16:35:46 -0500

    popup añadir

--- Evaluacion4/wwwroot/js/popUpWindowGoToCart.js
+++ Evaluacion4/wwwroot/js/popUpWindowGoToCart.js
@@ -1,9 +1,37 @@
 // <===================Para mostrar un alert de que se hizo la compra (momentaneo)=====================>
 var botonAñadirCarrito = document.getElementsByClassName("btn-agregar-carrito");
+var btnCerrarPopUp = document.querySelectorAll('.btn-cerrar');
+
 
 //Documentar y entender este codigo:
 [].forEach.call(botonAñadirCarrito, function(e) {
     e.onclick = function () {
-        alert("¡Se añadio al carrito!");
+        let contenedorModal = document.getElementById('contenedorModal');
+        mostrarPopUp(contenedorModal);
     }
 })
+
+// btnAddToCart.forEach(button => {
+//     button.addEventListener('click', () => {
+//         let contenedorModal = document.getElementById('contenedorModal');
+//         mostrarPopUp(contenedorModal);
+//     });
+// });
+
+btnCerrarPopUp.forEach(button => {
+    button.addEventListener('click', () => {
+        let btnCerrarPopUp = button.closest('.contenedor-modal');
+        cerrarPopUp(btnCerrarPopUp);
+    });
+});
+
+function mostrarPopUp(data) {
+    data.classList.toggle('active');
+    let bodyBlur = document.getElementById('bodyBlur');
+    bodyBlur.classList.toggle('active');
+}
+
+function cerrarPopUp(data) {
+    data.classList.remove('active');
+    bodyBlur.classList.remove('active');
+}
